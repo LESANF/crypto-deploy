c1f1ce8b597eb6a786626ead9cd43a7be8899ce1
LESANF <nagong1000@naver.com>
2021-12-20 20:44:09 +0900

    add price && info interface

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,130 @@
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    padding: 0 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    height: 15vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+    font-size: 48px;
+    color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
+interface RouteState {
+    coinId: string;
+}
+
+interface ITags {
+    coin_counter: number;
+    ico_counter: number;
+    id: string;
+    name: string;
+}
+
+interface IInfo {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    tags: ITags[];
+    team: object;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    links: object;
+    links_extended: object;
+    whitepaper: object;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface Iprice {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+        USD: {
+            ath_date: string;
+            ath_price: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+        };
+    };
+}
 
 function Coin() {
-    const { coinId } = useParams();
-    return <h1>coin: {coinId}</h1>;
+    const [loading, setLoading] = useState(true);
+    const [price, setPrice] = useState<Iprice>();
+    const [info, setInfo] = useState<IInfo>();
+    const state: RouteState = useLocation().state;
+
+    useEffect(() => {
+        (async () => {
+            const infoData = await (
+                await fetch(`https://api.coinpaprika.com/v1/coins/${state.coinId}`)
+            ).json();
+
+            const priceData = await (
+                await fetch(`https://api.coinpaprika.com/v1/tickers/${state.coinId}`)
+            ).json();
+            setInfo(infoData);
+            setPrice(priceData);
+            setLoading(false);
+        })();
+    }, []);
+
+    console.log(price);
+    return (
+        <Container>
+            <Header>
+                <Title>{state?.coinId || 'Loading...'}</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> : price?.quotes.USD.price}
+        </Container>
+    );
 }
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,11 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
     padding: 0 20px;
+    max-width: 480px;
+    margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -21,7 +24,8 @@ const Coin = styled.li`
     margin-bottom: 20px;
     a {
         padding: 20px;
-        display: block;
+        display: flex;
+        align-items: center;
         transition: color 0.2s ease-in;
     }
     &:hover {
@@ -36,49 +40,67 @@ const Title = styled.h1`
     color: ${(props) => props.theme.accentColor};
 `;
 
-const coins = [
-    {
-        id: 'btc-bitcoin',
-        name: 'Bitcoin',
-        symbol: 'BTC',
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: 'coin',
-    },
-    {
-        id: 'eth-ethereum',
-        name: 'Ethereum',
-        symbol: 'ETH',
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: 'coin',
-    },
-    {
-        id: 'bnb-binance-coin',
-        name: 'Binance Coin',
-        symbol: 'BNB',
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: 'coin',
-    },
-];
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
+`;
+
+const P = styled.p`
+    margin-top: 5px;
+`;
+
+interface CoinProps {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+}
 
 function Coins() {
+    const [coins, setCoins] = useState<CoinProps[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    const getApi = async () => {
+        const response = await fetch('https://api.coinpaprika.com/v1/coins');
+        const json = await response.json();
+        setCoins(json.slice(0, 100));
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getApi();
+    }, []);
+
     return (
         <Container>
             <Header>
                 <Title>코인</Title>
             </Header>
-            <CoinList>
-                {coins.map((coin) => (
-                    <Coin key={coin.id}>
-                        <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-                    </Coin>
-                ))}
-            </CoinList>
+            {loading ? (
+                <Loader>로딩 중...</Loader>
+            ) : (
+                <CoinList>
+                    {coins.map((coin) => (
+                        <Coin key={coin.id}>
+                            <Link to={`/${coin.id}`} state={{ coinId: coin.id }}>
+                                <Img
+                                    src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                                />
+                                <P>{coin.name} &rarr;</P>
+                            </Link>
+                        </Coin>
+                    ))}
+                </CoinList>
+            )}
         </Container>
     );
 }
