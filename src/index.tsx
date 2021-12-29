f509fe6b61f16c8199abec7fc333bb77b0a98271
LESANF <nagong1000@naver.com>
2021-12-29 20:44:22 +0900

    docs: add React-Query

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </QueryClientProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { fetchCoins } from '../api';
 
 const Container = styled.div`
     padding: 0 20px;
@@ -66,30 +68,17 @@ interface CoinProps {
 }
 
 function Coins() {
-    const [coins, setCoins] = useState<CoinProps[]>([]);
-    const [loading, setLoading] = useState(true);
-
-    const getApi = async () => {
-        const response = await fetch('https://api.coinpaprika.com/v1/coins');
-        const json = await response.json();
-        setCoins(json.slice(0, 100));
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        getApi();
-    }, []);
-
+    const { isLoading, data } = useQuery<CoinProps[]>('allCoins', fetchCoins);
     return (
         <Container>
             <Header>
                 <Title>코인</Title>
             </Header>
-            {loading ? (
+            {isLoading ? (
                 <Loader>로딩 중...</Loader>
             ) : (
                 <CoinList>
-                    {coins.map((coin) => (
+                    {data?.slice(0, 20).map((coin) => (
                         <Coin key={coin.id}>
                             <Link to={`/${coin.id}`} state={{ name: coin.name }}>
                                 <Img
