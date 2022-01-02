e2f20e3a412d036750c850cd1df4243c7fe7f306
LESANF <nagong1000@naver.com>
2022-01-02 17:14:58 +0900

    docs: Add Chart API

--- src/api.ts
+++ src/api.ts
@@ -11,3 +11,11 @@ export async function fetchCoinInfo(coinId: string) {
 export async function fetchCoinPrice(coinId: string) {
     return await (await fetch(`${BASE_URL}/tickers/${coinId}`)).json();
 }
+
+export async function fetchOHLCvalue(coinId: string) {
+    const endDate = Math.floor(Date.now() / 1000);
+    const startDate = endDate - 60 * 60 * 24 * 7 * 2;
+    return await (
+        await fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`)
+    ).json();
+}
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,13 @@
-function Chart() {
-    return <h1>Chart</h1>;
+import { useQuery } from 'react-query';
+import { fetchOHLCvalue } from '../api';
+
+interface ChartProps {
+    coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+    const { isLoading, data } = useQuery('ohlcv', () => fetchOHLCvalue(coinId));
+    return <h1>{coinId}</h1>;
 }
 
 export default Chart;
