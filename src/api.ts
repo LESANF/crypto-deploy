4bc43dfd53c5fa0132b8dd55d4d5fa8ccb73ce5f
LESANF <nagong1000@naver.com>
2022-01-06 06:15:00 +0900

    deploy

--- src/api.ts
+++ src/api.ts
@@ -1,21 +1,21 @@
-const BASE_URL = 'https://api.coinpaprika.com/v1';
-
 export async function fetchCoins() {
-    return await (await fetch(`${BASE_URL}/coins`)).json();
+    return await (await fetch(`${process.env.REACT_APP_BASE_URL}/coins`)).json();
 }
 
 export async function fetchCoinInfo(coinId: string) {
-    return await (await fetch(`${BASE_URL}/coins/${coinId}`)).json();
+    return await (await fetch(`${process.env.REACT_APP_BASE_URL}/coins/${coinId}`)).json();
 }
 
 export async function fetchCoinPrice(coinId: string) {
-    return await (await fetch(`${BASE_URL}/tickers/${coinId}`)).json();
+    return await (await fetch(`${process.env.REACT_APP_BASE_URL}/tickers/${coinId}`)).json();
 }
 
 export async function fetchOHLCvalue(coinId: string) {
     const endDate = Math.floor(Date.now() / 1000);
     const startDate = endDate - 60 * 60 * 24 * 7 * 2;
     return await (
-        await fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`)
+        await fetch(
+            `${process.env.REACT_APP_BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+        )
     ).json();
 }
